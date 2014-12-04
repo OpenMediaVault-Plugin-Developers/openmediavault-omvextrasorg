f3a04aed11edae5b06ef352b23eba40b7e3bfb5d
Aaron Murray <plugins@omv-extras.org>
2014-12-04 06:11:50 -0600

    allow none for reset perms shared folder

--- var/www/openmediavault/js/omv/module/admin/privilege/sharedfolder/ResetPerms.js
+++ var/www/openmediavault/js/omv/module/admin/privilege/sharedfolder/ResetPerms.js
@@ -60,70 +60,81 @@ Ext.define("OMV.module.admin.privilege.sharedfolder.ResetPerms", {
     getFormItems : function() {
         var me = this;
         return [{
-            xtype      : "sharedfoldercombo",
-            name       : "sharedfolderref",
-            fieldLabel : _("Shared Folder"),
-            allowNone  : true
-        },{
-            xtype       : "combo",
-            name        : "mode",
-            fieldLabel  : _("Permissions"),
-            queryMode   : "local",
-            store       : Ext.create("Ext.data.ArrayStore", {
-                fields : [ "value", "text" ],
-                data   : [
-                    [ "700", _("Administrator: read/write, Users: no access, Others: no access") ],
-                    [ "750", _("Administrator: read/write, Users: read-only, Others: no access") ],
-                    [ "770", _("Administrator: read/write, Users: read/write, Others: no access") ],
-                    [ "755", _("Administrator: read/write, Users: read-only, Others: read-only") ],
-                    [ "775", _("Administrator: read/write, Users: read/write, Others: read-only") ],
-                    [ "777", _("Everyone: read/write") ]
-                ]
-            }),
-            displayField    : "text",
-            valueField      : "value",
-            allowBlank      : false,
-            editable        : false,
-            showItemTooltip : true,
-            triggerAction   : "all",
-            value           : "775",
-            plugins         : [{
-                ptype : "fieldinfo",
-                text  : _("Reset folders and files to this file mode.")
+            xtype         : "fieldset",
+            title         : _("Reset Permissions"),
+            fieldDefaults : {
+                labelSeparator : ""
+            },
+            items         : [{
+                xtype      : "sharedfoldercombo",
+                name       : "sharedfolderref",
+                fieldLabel : _("Shared Folder"),
+                allowNone  : true,
+                allowBlank : true
+            },{
+                xtype       : "combo",
+                name        : "mode",
+                fieldLabel  : _("Permissions"),
+                queryMode   : "local",
+                store       : Ext.create("Ext.data.ArrayStore", {
+                    fields : [ "value", "text" ],
+                    data   : [
+                        [ "700", _("Administrator: read/write, Users: no access, Others: no access") ],
+                        [ "750", _("Administrator: read/write, Users: read-only, Others: no access") ],
+                        [ "770", _("Administrator: read/write, Users: read/write, Others: no access") ],
+                        [ "755", _("Administrator: read/write, Users: read-only, Others: read-only") ],
+                        [ "775", _("Administrator: read/write, Users: read/write, Others: read-only") ],
+                        [ "777", _("Everyone: read/write") ]
+                    ]
+                }),
+                displayField    : "text",
+                valueField      : "value",
+                allowBlank      : false,
+                editable        : false,
+                showItemTooltip : true,
+                triggerAction   : "all",
+                value           : "775",
+                plugins         : [{
+                    ptype : "fieldinfo",
+                    text  : _("Reset folders and files to this file mode.")
+                }]
+            },{
+                xtype      : "checkbox",
+                name       : "clearacl",
+                fieldLabel : _("Clear ACLs"),
+                boxLabel   : _("Clear all ACLs."),
+                checked    : false
             }]
-        },{
-            xtype      : "checkbox",
-            name       : "clearacl",
-            fieldLabel : _("Clear ACLs"),
-            boxLabel   : _("Clear all ACLs."),
-            checked    : false
         }];
     },
 
     onResetPermsButton: function() {
         var me = this;
         me.doSubmit();
-        var wnd = Ext.create("OMV.window.Execute", {
-            title      : _("Reset permissions ..."),
-            rpcService : "OmvExtrasOrg",
-            rpcMethod  : "doResetPerms",
-            hideStartButton : true,
-            hideStopButton  : true,
-            listeners       : {
-                scope     : me,
-                finish    : function(wnd, response) {
-                    wnd.appendValue(_("Done..."));
-                    wnd.setButtonDisabled("close", false);
-                },
-                exception : function(wnd, error) {
-                    OMV.MessageBox.error(null, error);
-                    wnd.setButtonDisabled("close", false);
+        var folder = me.findField("sharedfolderref").getValue();
+        if (folder != "") {
+            var wnd = Ext.create("OMV.window.Execute", {
+                title      : _("Reset permissions ..."),
+                rpcService : "OmvExtrasOrg",
+                rpcMethod  : "doResetPerms",
+                hideStartButton : true,
+                hideStopButton  : true,
+                listeners       : {
+                    scope     : me,
+                    finish    : function(wnd, response) {
+                        wnd.appendValue(_("Done..."));
+                        wnd.setButtonDisabled("close", false);
+                    },
+                    exception : function(wnd, error) {
+                        OMV.MessageBox.error(null, error);
+                        wnd.setButtonDisabled("close", false);
+                    }
                 }
-            }
-        });
-        wnd.setButtonDisabled("close", true);
-        wnd.show();
-        wnd.start();
+            });
+            wnd.setButtonDisabled("close", true);
+            wnd.show();
+            wnd.start();
+        }
     }
 });
 
