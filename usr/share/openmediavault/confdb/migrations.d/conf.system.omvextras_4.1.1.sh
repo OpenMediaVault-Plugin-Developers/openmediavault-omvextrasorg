afd211e2f47b8f6303ae541a2b16cbd7466b6d19
Aaron Murray <plugins@omv-extras.org>
2017-12-10 10:26:38 -0600

    update teamviewer repo url

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.system.omvextras_4.1.1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+# @copyright Copyright (c) 2009-2013 Volker Theile
+# @copyright Copyright (c) 2013-2017 OpenMediaVault Plugin Developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+omv_config_update "/config/system/omvextras/repos/repo[uuid='9b49b9a4-82d8-11e7-b73c-c7a588ae65a8']/repo1" "deb http://linux.teamviewer.com/deb stable main"
+
+# make config file
+omv-mkconf apt
+
+exit 0
